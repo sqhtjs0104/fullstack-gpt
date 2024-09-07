5649fb4315c02c72cd0a6aecb34ac5b77e52db7e
sqhtjs0104 <sqhtjs0104@gmail.com>
2024-09-08 02:31:25 +0900

    Quiz

--- Home.py
+++ Home.py
@@ -14,8 +14,8 @@ Welcome to my FullstackGPT Portfolio!
 Here are the apps I made:
             
 - [x] [DocumentGPT](/DocumentGPT)
-- [ ] [PrivateGPT](/PrivateGPT)
-- [ ] [QuizGPT](/QuizGPT)
+- [x] [PrivateGPT](/PrivateGPT)
+- [x] [QuizGPT](/QuizGPT)
 - [ ] [SiteGPT](/SiteGPT)
 - [ ] [MeetingGPT](/MeetingGPT)
 - [ ] [InvestorGPT](/InvestorGPT)
